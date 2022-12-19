23ea4f5a0f0587fae20494f931e8100b857795cf
bohyeonp <219031@naver.com>
2022-12-19 22:57:11 +0900

    api/adaptor.api.js : createUserWithEmailAndPasswordApi 함수 생성

--- src/api/adaptor.api.js
+++ src/api/adaptor.api.js
@@ -1,18 +1,30 @@
 import store from "../app/store";
-import {setList} from "../app/slice"
-import Api from "../api/api";
+import {setModalDefault} from "../app/slice";
+import {auth, firestore} from "../firebase/Firebase";
+import {createUserWithEmailAndPassword} from "firebase/auth";
 
-const api = new Api();
-
-export const exampleApi = (params, callback) => {
-    return api.exampleApi(params).then(data => {
-        // store.dispatch(setList(data.data)) // 바로 redux 저장 필요시에만 작성
-        callback("", data.data); //화면단으로 데이터 전송 필요시에만 작성
-    })
-};
-
-export const sampleApi = () => {
-    return api.sampleApi().then(data => {
-        store.dispatch(setList(data.data)) // 바로 redux 저장 필요시에만 작성
-    })
+export const createUserWithEmailAndPasswordApi = (values) => {
+    const user = firestore.collection("user");
+    return createUserWithEmailAndPassword(auth, values.user.email, values.user.password)
+        .then((userCredential) => {
+            const userInfo = userCredential.user;
+            user.doc(userInfo.uid).set({
+                ...values.user,
+                photoURL : "https://cdn.pixabay.com/photo/2021/02/12/07/03/icon-6007530_1280.png"
+            });
+        })
+        .catch((error) => {
+            console.error('이메일 가입시 에러 : ', error);
+            switch(error.code){
+                case "auth/email-already-in-use":
+                    store.dispatch(setModalDefault({show: true, type: "email-already-in-use"}));
+                    break;
+                case "auth/weak-password":
+                    store.dispatch(setModalDefault({show: true, type: "weak-password"}));
+                    break;
+                default:
+                    store.dispatch(setModalDefault({show: true, type: "join-fail"}));
+                    break;
+            }
+        });
 };
