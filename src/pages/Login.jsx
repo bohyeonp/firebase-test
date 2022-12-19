8d82f2d5a2ad0c47305948825352dfd8084d723b
bohyeonp <219031@naver.com>
2022-12-19 23:26:20 +0900

    Login.jsx - Link -> useNavigate - 폼 퍼블리싱 수정 - placeholder 문구 수정 - 패스워드 : Input -> Input.Password - 회원가입 아이콘 : Avatar -> Button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import { LockOutlined, UserOutlined, GithubOutlined, GoogleOutlined, MailOutlined, UserAddOutlined, MessageOutlined, ReadOutlined} from '@ant-design/icons';
-import { Button, Form, Input, Avatar} from 'antd';
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+import { LockOutlined, UserOutlined, GithubOutlined, GoogleOutlined, UserAddOutlined, MessageOutlined, ReadOutlined, MailOutlined} from '@ant-design/icons';
+import { Button, Form, Input} from 'antd';
 import {auth} from "../firebase/Firebase"
 import {setModalDefault} from "../app/slice";
 import { signInWithEmailAndPassword} from "firebase/auth";
 import {useDispatch} from "react-redux";
 
 const Login = () => {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const layout = {
         labelCol: {
@@ -46,57 +47,97 @@ const Login = () => {
 
     return (
         <>
-            <Form
-                {...layout}
-                style={{maxWidth: '800px', margin: '0 auto', paddingTop: '40px'}}
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinishEP}
-            >
-                <Form.Item
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Email!',
-                        },
-                    ]}
+            <div>
+                <Form
+                    {...layout}
+                    style={{maxWidth: '600px', margin: '0 auto', paddingTop: '40px'}}
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinishEP}
                 >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Password!',
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        placeholder="Password"
-                        type="password"
-                        autoComplete="on"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Login
-                    </Button>
-                </Form.Item>
-                <div >
-                    <Link to={"/join/detail"}><Avatar style={{marginRight : '15px'}} size="large" icon={<UserAddOutlined />}/></Link>
-                    <Link to={"/join/simple"}><Avatar style={{marginRight : '15px'}} size="large" icon={<MailOutlined />}/></Link>
-                    <Avatar style={{marginRight : '15px'}} size="large" icon={<GithubOutlined />}/>
-                    <Avatar style={{marginRight : '15px'}} size="large" icon={<GoogleOutlined />}/>
-                    <Avatar style={{marginRight : '15px'}} size="large" icon={<MessageOutlined />}/>
-                    <Avatar size="large" icon={<ReadOutlined />}/>
-                </div>
-            </Form>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Email!',
+                            },
+                        ]}
+                    >
+                        <Input placeholder="이메일을 입력해주세요." prefix={<UserOutlined className="site-form-item-icon" style={{ color: 'rgba(0, 0, 0, 0.25)' }}/>}/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined className="site-form-item-icon" style={{ color: 'rgba(0, 0, 0, 0.25)' }}/>}
+                            placeholder="비밀번호를 입력해주세요."
+                            type="password"
+                            autoComplete="on"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 4,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+            <div className='login-icons' style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button
+                    size="large"
+                    shape="circle"
+                    icon={<UserAddOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                    onClick={() => navigate('/join/detail')}
+                />
+                <Button
+                    style={{ marginLeft: '10px' }}
+                    size="large"
+                    shape="circle"
+                    icon={<MailOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                    onClick={() => navigate('/join/simple')}
+                />
+                <Button
+                    style={{ marginLeft: '10px' }}
+                    size="large"
+                    shape="circle"
+                    icon={<GithubOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                />
+                <Button
+                    style={{ marginLeft: '10px' }}
+                    size="large"
+                    shape="circle"
+                    icon={<GoogleOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                />
+                <Button
+                    style={{ marginLeft: '10px' }}
+                    size="large"
+                    shape="circle"
+                    icon={<MessageOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                />
+                <Button
+                    style={{ marginLeft: '10px' }}
+                    size="large"
+                    shape="circle"
+                    icon={<ReadOutlined  style={{ color: '#1890ff', fontSize: '24px' }} />}
+                />
+            </div>
         </>
     )
 }
