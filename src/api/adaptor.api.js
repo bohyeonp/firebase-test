565ce8dc01d0b16981d28a8c7d378a5d136b3004
bohyeonp <219031@naver.com>
2023-01-10 20:15:35 +0900

    deleteUserApi / updatePasswordApi / updateProfileApi / reProfileApi 추가

--- src/api/adaptor.api.js
+++ src/api/adaptor.api.js
@@ -1,16 +1,17 @@
 import store from "../app/store";
-import {setModalDefault} from "../app/slice";
+import {setModalDefault, setUserProfile} from "../app/slice";
 import {auth, firestore} from "../firebase/Firebase";
-import {createUserWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, deleteUser, EmailAuthProvider, updatePassword, reauthenticateWithCredential} from "firebase/auth";
 
 export const createUserWithEmailAndPasswordApi = (values) => {
+    const {email, password} = values.user;
     const user = firestore.collection("user");
-    return createUserWithEmailAndPassword(auth, values.user.email, values.user.password)
+    createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const userInfo = userCredential.user;
             user.doc(userInfo.uid).set({
                 ...values.user,
-                photoURL : "https://cdn.pixabay.com/photo/2021/02/12/07/03/icon-6007530_1280.png"
+                photoNum : "0"
             });
         })
         .catch((error) => {
@@ -19,12 +20,72 @@ export const createUserWithEmailAndPasswordApi = (values) => {
                 case "auth/email-already-in-use":
                     store.dispatch(setModalDefault({show: true, type: "email-already-in-use"}));
                     break;
-                case "auth/weak-password":
-                    store.dispatch(setModalDefault({show: true, type: "weak-password"}));
-                    break;
                 default:
                     store.dispatch(setModalDefault({show: true, type: "join-fail"}));
                     break;
             }
         });
+};
+
+export const deleteUserApi = () => {
+    const uid = store.getState().common.userProfile.uid;
+    const user = firestore.collection("user");
+    deleteUser(auth.currentUser).then(() => {
+        user.doc(uid).delete();
+    }).catch((error) => {
+        store.dispatch(setModalDefault({show: true, type: "delete-fail"}));
+    });
+};
+
+export const updatePasswordApi = (password) => {
+    const userProfile = store.getState().common.userProfile
+    const credential = EmailAuthProvider.credential(
+        userProfile.email,
+        password.current
+    );
+    console.log(credential)
+    const uid = store.getState().common.userProfile.uid;
+    const user = firestore.collection("user");
+    updatePassword(auth.currentUser, password.new).then(() => {
+        console.log("비밀번호 변경 성공")
+        user.doc(uid).update({ password : password.new});
+        store.dispatch(setModalDefault({show: true, type: "pw-update-success"}));
+    }).catch((error) => {
+        console.log("비밀번호 변경 실패")
+        reauthenticateWithCredential(auth.currentUser, credential).then(() => {
+            console.log("사용자 재인증 성공")
+            updatePasswordApi(password)
+        }).catch((error) => {
+            console.log("사용자 재인증 실패")
+            store.dispatch(setModalDefault({show: true, type: "pw-update-fail"}));
+        });
+
+    });
+};
+
+export const updateProfileApi = (values) => {
+    const {name, email, photo, birth, phone} = values.user;
+    const uid = store.getState().common.userProfile.uid;
+    const user = firestore.collection("user");
+    user.doc(uid).update({
+        name : name,
+        //email: email,
+        photoNum : photo,
+        birth : birth || "",
+        phone : phone || ""
+    });
+    store.dispatch(setModalDefault({show: true, type: "profile-update-success"}));
+    reProfileApi();
+};
+
+export const reProfileApi = (userId) => {
+    const user = firestore.collection("user");
+    const uid = userId || store.getState().common.userProfile.uid;
+    user.get().then((doc) => {
+        doc.forEach((doc2)=>{
+            if(doc2.id === uid){
+                store.dispatch(setUserProfile({...doc2.data(), ...{uid : uid}}));
+            }
+        })
+    });
 };
