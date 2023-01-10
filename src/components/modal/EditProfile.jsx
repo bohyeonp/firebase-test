99224e32e520914819a79fadd9132b0dd339165b
bohyeonp <219031@naver.com>
2023-01-10 20:21:23 +0900

    프로필 업데이트 기능 추가 (email 제외 작업 완)

--- src/components/modal/EditProfile.jsx
+++ src/components/modal/EditProfile.jsx
@@ -1,59 +1,159 @@
 import React from 'react';
-import {Form, Input, Upload} from "antd";
+import {Form, Input, Button, Radio} from "antd";
 import {useSelector} from "react-redux";
 import {selectUserProfile} from "../../app/slice";
+import {GiftOutlined, MailOutlined, PhoneOutlined, UserOutlined} from "@ant-design/icons";
+import {checkBirth, checkName, checkPhoneNumber} from "../../utils/utilCommon";
+import "../../assets/css/styles.css"
+import {updateProfileApi} from "../../api/adaptor.api";
 
 const EditProfile = () => {
     const userProfile = useSelector(selectUserProfile);
 
+    const imageArray = ["0","1","2","3","4","5","6","7","8"]
+    const layout = {
+        labelCol: {
+            span: 4,
+        },
+        wrapperCol: {
+            span: 16,
+        },
+    };
+    const validateMessages = {
+        required: '${label} is required!',
+        types: {
+            email: '${label} is not a valid email!',
+        }
+    };
+
+    const validateName = (_, value) => {
+        if (!value || checkName(value)) {
+            return Promise.resolve();
+        } else {
+            return Promise.reject(new Error(_.message));
+        }
+    }
+
+    const validateBirth = (_, value) => {
+        if (!value || checkBirth(value)) {
+            return Promise.resolve();
+        } else {
+            return Promise.reject(new Error(_.message));
+        }
+    }
+
+    const validatePhone = (_, value) => {
+        if (!value || checkPhoneNumber(value)) {
+            return Promise.resolve();
+        } else {
+            return Promise.reject(new Error(_.message));
+        }
+    }
+
+    const onFinish = (values) => {
+        updateProfileApi(values);
+    };
+
+
     return (
         <>
-            <Upload
-                name="avatar"
-                listType="picture-card"
-                className="avatar-uploader"
-                showUploadList={false}
-                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            <Form
+                {...layout}
+                initialValues={{
+                    user : {
+                        name : userProfile.name,
+                        email : userProfile.email,
+                        birth : userProfile.birth,
+                        phone : userProfile.phone,
+                        photo : userProfile.photoNum
+                    }
+                }}
+                onFinish={onFinish}
+                validateMessages={validateMessages}
             >
-                <img
-                    src={userProfile.photoURL}
-                    alt="avatar"
-                    style={{
-                        width: '100%',
-                        height: '100%'
-                    }}
-                />
-            </Upload>
-            <Form name="nest-messages" style={{marginTop : '30px'}}>
                 <Form.Item
                     name={['user', 'name']}
                     label="Name"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                        {
+                            validator: validateName,
+                            message: '이름에 숫자, 특수문자는 사용할 수 없습니다.'
+                        }
+                    ]}
                 >
-                    <Input defaultValue={userProfile.name} />
+                    <Input placeholder="이름을 입력해주세요." prefix={<UserOutlined style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}/>
                 </Form.Item>
                 <Form.Item
                     name={['user', 'email']}
                     label="Email"
                     rules={[
                         {
-                            type: 'email',
+                            required: true,
                         },
+                        {
+                            type: 'email',
+                            message: '이메일 형식에 맞게 작성해주세요.'
+                        }
                     ]}
                 >
-                    <Input defaultValue={userProfile.email} />
+                    <Input placeholder="이메일을 입력해주세요." prefix={<MailOutlined style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}/>
+                </Form.Item>
+                <Form.Item
+                    name={['user', 'photo']}
+                    label="Photo"
+                    rules={[
+                        {
+                            required: true,
+                        }
+                    ]}
+                >
+                    <Radio.Group className="radio-custom">
+                        {
+                            imageArray.map((item, index) => (
+                                <Radio value={item} key={index} className={item === userProfile.photoNum && "ant-radio-wrapper-checked-origin"}> <img src={require(`../../assets/images/photo_${index}.png`)} alt=""/> </Radio>
+                            ))
+                        }
+                    </Radio.Group>
                 </Form.Item>
                 <Form.Item
                     name={['user', 'birth']}
                     label="Birth"
+                    rules={[
+                        {
+                            validator: validateBirth,
+                            message: '생년월일은 \'YYYYMMDD\' 형식으로 숫자만 입력해주세요.'
+                        }
+                    ]}
                 >
-                    <Input defaultValue={userProfile.birth} />
+                    <Input placeholder="생년월일을 입력해주세요." prefix={<GiftOutlined style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}/>
                 </Form.Item>
                 <Form.Item
                     name={['user', 'phone']}
                     label="Phone"
+                    rules={[
+                        {
+                            validator: validatePhone,
+                            message: '숫자만 입력해주세요.'
+                        }
+                    ]}
                 >
-                    <Input defaultValue={userProfile.phone} />
+                    <Input placeholder="휴대폰 번호를 입력해주세요." prefix={<PhoneOutlined style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}/>
                 </Form.Item>
+                <Form.Item
+                    name="submit"
+                    wrapperCol={{
+                        offset: 4,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Edit
+                    </Button>
+                </Form.Item>
+
             </Form>
         </>
     )
