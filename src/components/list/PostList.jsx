3f09680d89e85011bb7663560e212e1c9f21c0f7
bohyeonp <219031@naver.com>
2023-01-26 01:42:50 +0900

    포스트 리스트 업로드 컴포넌트 제거

--- src/components/list/PostList.jsx
+++ src/components/list/PostList.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Table, Tag, Space, Upload, Button, message} from "antd";
+import React, {useEffect} from 'react';
+import {Button} from "antd";
 import {PlusOutlined, UploadOutlined} from "@ant-design/icons";
 import {useDispatch, useSelector} from "react-redux";
 import {selectUserProfile, setModalDefault} from "../../app/slice";
@@ -31,79 +31,18 @@ const PostList = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const userProfile = useSelector(selectUserProfile);
+
     useEffect(()=> {
         console.log("포스트 리스트")
     },[]);
 
-    const [imageUrl, setImageUrl] = useState(userProfile.photoNum);
-
-    useEffect(()=> {
-        console.log("마이")
-    },[]);
-
     const uploadPost = () => dispatch(setModalDefault({show: true, type: 'upload-post'}));
-
-    const getBase64 = (img, callback) => {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsDataURL(img);
-    };
-
-    const beforeUpload = (file) => {
-        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-        if (!isJpgOrPng) {
-            message.error('You can only upload JPG/PNG file!');
-        }
-        const isLt2M = file.size / 1024 / 1024 < 2;
-        if (!isLt2M) {
-            message.error('Image must smaller than 2MB!');
-        }
-        return isJpgOrPng && isLt2M;
-    };
-
-    const handleChange = (info) => {
-        // Get this url from response in real world.
-        getBase64(info.file.originFileObj, (url) => {
-            // setLoading(false);
-            console.log(url)
-            setImageUrl(url);
-        });
-    };
-
-    const props = {
-        name: 'file',
-        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        headers: {
-            authorization: 'authorization-text',
-        },
-        onChange(info) {
-            if (info.file.status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            if (info.file.status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully`);
-            } else if (info.file.status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
-            }
-        },
-    };
 
     return (
         <div style={{marginLeft : '5%'}}>
             {userProfile.list?.post.length === 0
                 ? <div style={{textAlign:'center', fontSize: '15px', marginTop: '100px'}}>
                     <p>첫 포스트를 업로드해보세요.</p>
-                    {/*<Upload*/}
-                    {/*    name="avatar"*/}
-                    {/*    listType="picture-card"*/}
-                    {/*    className="avatar-uploader"*/}
-                    {/*    showUploadList={false}*/}
-                    {/*    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"*/}
-                    {/*    beforeUpload={beforeUpload}*/}
-                    {/*    onChange={handleChange}*/}
-                    {/*>*/}
-                    {/*    <div>업로드</div>*/}
-                    {/*</Upload>*/}
                     <div onClick={uploadPost}>
                         <Button icon={<UploadOutlined />}>UPLOAD</Button>
                     </div>
